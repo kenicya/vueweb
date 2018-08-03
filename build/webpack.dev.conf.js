107d3bb826258a65bba48aa61b4e24e676972fbc
fenghuiya <10385@sangfor.com>
2018-08-03 19:43:10 +0800

    去掉console

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -9,7 +9,6 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
-console.log('aaa')
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
